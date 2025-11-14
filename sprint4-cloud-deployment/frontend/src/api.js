9068c0a8ff4be2172573ee8134ea5bdb6d390613
Dr. Devine <phlosso4@gmail.com>
2025-11-14 11:04:21 -0500

    wipe sprint 4

--- sprint4-cloud-deployment/frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-// src/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // e.g., http://localhost:5080 or https://<api>.azurewebsites.net
-  withCredentials: true                  // send/receive HTTP-only auth cookie
-});
-
-export default api;
